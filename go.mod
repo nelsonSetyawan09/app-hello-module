acc3edbfd1ef0b0db95959cb5550df46ea48a403
User <user@192.168.1.2>
2022-06-11 13:03:09 +0800

    app say hello

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/nelsonSetyawan09/app-hello-module
+
+go 1.18
+
+require github.com/nelsonSetyawan09/hello-go-module v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_hello "github.com/nelsonSetyawan09/hello-go-module"
+)
+
+func main() {
+	fmt.Println(go_hello.SayHello())
+}
